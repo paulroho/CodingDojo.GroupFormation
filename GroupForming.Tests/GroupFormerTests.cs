3eed83e00767ae4eeb9dfbce64026c2a2c6ee42b
Paul Rohorzka <paul@paulroho.com>
2015-05-08 17:05:28 +0200

    Generalizing first test with autofixture and providing a simple implementation

--- GroupForming.Tests/GroupFormerTests.cs
+++ GroupForming.Tests/GroupFormerTests.cs
@@ -1,6 +1,7 @@
 ﻿using System.Linq;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ploeh.AutoFixture;
 
 namespace GroupForming.Tests
 {
@@ -10,15 +11,18 @@ namespace GroupForming.Tests
         [TestMethod]
         public void Shuffle_WithMembersForJustOneGroup_GivesASingleGroupContainingAllMembers()
         {
+            var fixture = new Fixture();
+            var member1 = fixture.Create<string>("Member");
+            var member2 = fixture.Create<string>("Member");
             var former = new GroupFormer();
-            former.AddMember("Michi");
-            former.AddMember("Dodo");
+            former.AddMember(member1);
+            former.AddMember(member2);
 
             // Act
             former.Shuffle();
 
             former.Groups.Should().HaveCount(1, "there should be just a single group");
-            former.Groups.First().Members.ShouldAllBeEquivalentTo(new[] { "Michi", "Dodo" });
+            former.Groups.First().Members.ShouldAllBeEquivalentTo(new[] { member1, member2 });
         }
     }
 }
--- GroupForming/GroupFormer.cs
+++ GroupForming/GroupFormer.cs
@@ -1,22 +1,33 @@
 ﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
 
 namespace GroupForming
 {
     public class GroupFormer
     {
+        private readonly IList<string> _members = new List<string>();
+
         public void AddMember(string member)
         {
+            _members.Add(member);
         }
 
         public void Shuffle()
         {
         }
 
-        public IList<Group> Groups { get { return new[] { new Group() }; } }
+        public IList<Group> Groups { get { return new[] { new Group(_members) }; } }
     }
 
     public class Group
     {
-        public IList<string> Members { get { return new[] { "Dodo", "Michi" }; } }
+        private readonly List<string> _members = new List<string>();
+
+        public Group(IEnumerable<string> members)
+        {
+            _members.AddRange(members);
+        }
+
+        public IList<string> Members { get { return _members; } }
     }
 }
