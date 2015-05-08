ca6cf8927cb2cbf5c24eb5488aac0ad196d6557b
Paul Rohorzka <paul@paulroho.com>
2015-05-08 16:47:16 +0200

    Solution with first unit test

---
+++ GroupForming/GroupFormer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace GroupForming
+{
+    public class GroupFormer
+    {
+        public void AddMember(string member)
+        {
+        }
+
+        public void Shuffle()
+        {
+        }
+
+        public IList<Group> Groups { get { return new[] { new Group() }; } }
+    }
+
+    public class Group
+    {
+        public IList<string> Members { get { return new[] { "Dodo", "Michi" }; } }
+    }
+}
